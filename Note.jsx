c103667ceba84c151911e8f927dea417b95a0074
mouazourassen18 <54610297+mouazourassen18@users.noreply.github.com>
2021-08-21 11:39:34 -0400

    Initial commit

---
+++ Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div class="note">
+      <h1> this a heading </h1>
+      <p> Tis is a PARAGRAPH </p>
+    </div>
+  );
+}
+export default Note;
---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+var date = new Date().getFullYear();
+function Footer() {
+  return <footer> CopyRight 2021 </footer>;
+}
+
+export default Footer;
